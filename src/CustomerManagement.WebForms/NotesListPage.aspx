da4f6039453ff014ccf8bc809dd9ffa3d12fc5e8
alevshinskii <66357905+alevshinskii@users.noreply.github.com>
2022-08-31 12:09:57 +0300

    feat: notes list page

---
+++ src/CustomerManagement.WebForms/NotesListPage.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="Notes List" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="NotesListPage.aspx.cs" Inherits="CustomerManagement.WebForms.NotesListPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %></h2>
+    <a href="NoteAdd?CustomerId=<%=CustomerId %>" class="m">Add new Note</a>
+    <table class="table table-hover table-striped">
+        <thead>
+        <tr>
+            <td>Id</td>
+            <td>CustomerId</td>
+            <td>Text</td>
+
+            <td></td>
+            <td></td>
+        </tr>
+        </thead>
+        <% foreach (var note in NotesList)
+           { %>
+
+            <tr>
+                <td>
+                    <%= note.Id %>
+                </td>
+                <td>
+                    <%= note.CustomerId %>
+                </td>
+                <td>
+                    <%= note.Text %>
+                </td>
+
+                <td>
+                    <a href="NoteEdit?Id=<%=note.Id %>">Edit</a>
+                </td>
+                <td>
+                    <a href="NoteDelete?Id=<%=note.Id %>">Delete</a>
+                </td>
+            </tr>
+        <%
+           } %>
+    </table>
+</asp:Content>
---
+++ src/CustomerManagement.WebForms/NotesListPage.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CustomerManagement.Entities;
+using CustomerManagement.Interfaces;
+using CustomerManagement.Repositories;
+
+namespace CustomerManagement.WebForms
+{
+    public partial class NotesListPage : Page
+    {
+        public IRepository<Customer> CustomerRepository { get; } = new CustomerRepository();
+        public IRepository<Note> NoteRepository { get; } = new NoteRepository();
+        public List<Note> NotesList { get; private set; } = new List<Note>();
+        public int CustomerId { get; private set; } = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetCustomerIdFromQuery();
+            GetNotes(CustomerId);
+        }
+        public void GetCustomerIdFromQuery()
+        {
+            try
+            {
+                var id = Request.QueryString["CustomerId"];
+                if (int.TryParse(id, out int parsedId))
+                {
+                    if (CustomerRepository.Read(parsedId) != null)
+                    {
+                        CustomerId=parsedId;
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Response.Redirect("NotFound.aspx");
+            }
+            
+            Response.Redirect("NotFound.aspx");
+        }
+        public void GetNotes(int customerId)
+        {
+            NotesList = NoteRepository.ReadAll(customerId);
+        }
+    }
+}
